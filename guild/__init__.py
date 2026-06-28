560584a729e8380ee56c9d41fef7a5c40c6e1ede
newhrmunna2027-pixel <newhrmunna2027@gmail.com>
2026-06-29 03:32:36 +0600

    Initialize guild module and import operations

---
+++ guild/__init__.py
@@ -0,0 +1,9 @@
+# filepath: guild/__init__.py
+# -*- coding: utf-8 -*-
+
+from guild.operations import (
+    get_clan_info_by_id,
+    get_guild_member_list,
+    request_join_clan,
+    quit_current_clan
+)
