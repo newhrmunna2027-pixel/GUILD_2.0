d29d388622ec70354e0875710749f8d7f25f01c1
newhrmunna2027-pixel <newhrmunna2027@gmail.com>
2026-06-29 03:31:09 +0600

    Initialize friend module with operations import

---
+++ friend/__init__.py
@@ -0,0 +1,12 @@
+# filepath: friend/__init__.py
+# -*- coding: utf-8 -*-
+
+from friend.operations import (
+    get_active_friend_list,
+    add_target_friend,
+    delete_active_friend,
+    get_pending_request_list,
+    accept_friend_request,
+    reject_friend_request,
+    check_duo_native
+)
